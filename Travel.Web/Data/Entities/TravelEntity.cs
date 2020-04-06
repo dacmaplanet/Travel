1f391e60de7c7b6bc6798d0cb0e1feb8e5533aa3
DANNY ALEXANDER CASTRO MACIAS <dannycastro42459@correo.itm.edu.co>
2020-04-06 16:10:18 -0500

    Improve TravelEntity

--- Travel.Web/Data/Entities/TravelEntity.cs
+++ Travel.Web/Data/Entities/TravelEntity.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Travel.Web.Data.Entities
 {
@@ -10,5 +11,27 @@ namespace Travel.Web.Data.Entities
         [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string Reservation { get; set; }
 
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}" , ApplyFormatInEditMode = false)]
+        public DateTime StartDate{ get; set; }
+
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime StartDateLocal => StartDate.ToLocalTime();
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime EndDateLocal => EndDate.ToLocalTime();
+
+        [StringLength(12, MinimumLength = 4, ErrorMessage = "The {0} field must have {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "City Name")]
+        public string City { get; set; }
     }
 }
---
+++ Travel.Web/Migrations/20200406151643_TravelEntitynewAtributes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Travel.Web.Migrations
+{
+    public partial class TravelEntitynewAtributes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Reservation",
+                table: "Travels",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 6);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Travels",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Travels",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Travels",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Travels");
+
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Travels");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Travels");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Reservation",
+                table: "Travels",
+                maxLength: 6,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 12);
+        }
+    }
+}
